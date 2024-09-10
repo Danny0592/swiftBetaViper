299e5a6c458b5aa70e056ba801ccfe287902ebe0
Danny0592 <Dortiz.millan@gmail.com>
2024-09-09 20:24:58 -0600

    viewModel

---
+++ swiftBetaViper/ListOfViper/View/ViewModel/Mapper.swift
@@ -0,0 +1,8 @@
+//
+//  mapper.swift
+//  swiftBetaViper
+//
+//  Created by daniel ortiz millan on 09/09/24.
+//
+
+import Foundation
---
+++ swiftBetaViper/ListOfViper/View/ViewModel/ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  viewModel.swift
+//  swiftBetaViper
+//
+//  Created by daniel ortiz millan on 09/09/24.
+//
+
+import Foundation
+
+struct viewModel {
+    var title: String
+    var overview: String
+    var imageURL: URL?
+    
+}
