4c36ff86aa75a9a5daedbe2e513e4c3941f914f1
Danny0592 <Dortiz.millan@gmail.com>
2024-09-10 18:54:45 -0600

    request detail interactor

--- swiftBetaViper/DetailMovie/DetailInteractor.swift
+++ swiftBetaViper/DetailMovie/DetailInteractor.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+protocol DetailInteractable: AnyObject {
+    func getDetailMovie (withId id: String) async -> DetailMovieEntity
+}
+
+class DetailInteractor: DetailInteractable {
+    func getDetailMovie (withId id: String) async -> DetailMovieEntity {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(id)?api_key=9b613bd3667f7fd328e037601b2f457b")!
+        let (data, _) = try! await URLSession.shared.data(from: url)
+        let JsonDecoder = JSONDecoder()
+        JsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try! JsonDecoder.decode(DetailMovieEntity.self, from: data)
+    }
+}
