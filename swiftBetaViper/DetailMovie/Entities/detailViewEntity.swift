025860cad08fd9130354ffe24e2006c32c6334f2
Danny0592 <Dortiz.millan@gmail.com>
2024-09-10 18:35:57 -0600

    detailViewEntity

---
+++ swiftBetaViper/DetailMovie/Entities/detailViewEntity.swift
@@ -0,0 +1,19 @@
+//
+//  detailViewEntity.swift
+//  swiftBetaViper
+//
+//  Created by daniel ortiz millan on 10/09/24.
+//
+
+import Foundation
+
+struct DetailMovieEntity: Decodable {
+    let title: String
+    let overview: String
+    let backdropPath: String
+    let status: String
+    let releaseDate: String
+    let voteAverage: Double
+    let voteCount: Int
+    
+}
