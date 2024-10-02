55235c895952d4ce6f41456bfab32e05de4b1d6c
Danny0592 <Dortiz.millan@gmail.com>
2024-10-02 01:09:43 -0600

    DetailRouter

--- swiftBetaViper/DetailMovie/DetailRouter.swift
+++ swiftBetaViper/DetailMovie/DetailRouter.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import UIKit
+
+protocol DetailRouting: AnyObject {
+    func showDetail(fromViewViewController: UIViewController,
+                    withMovieId movieId: String)
+}
+
+class DetailRouter: DetailRouting {
+    func showDetail(fromViewViewController: UIViewController,
+                    withMovieId movieId: String) {
+        let interactor = DetailInteractor()
+        let  presenter = DetailPresenter(movieId: movieId,
+                                               interactor: interactor,
+                                               mapper: MapperDetailMovieViewModel())
+        let view = DetailView(presenter: presenter)
+        presenter.ui = view
+        
+        fromViewViewController.present(view, animated: true)
+    }
+    
+}
