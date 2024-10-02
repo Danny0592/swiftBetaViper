33f082271e5ba3db422936be2cf911c7bc82748d
Danny0592 <Dortiz.millan@gmail.com>
2024-10-02 00:04:40 -0600

    respaldo

--- swiftBetaViper/DetailMovie/DetailView.swift
+++ swiftBetaViper/DetailMovie/DetailView.swift
@@ -46,4 +46,40 @@ class DetailView: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setupViews()
+        presenter.onViewAppear()
+    }
+    
+    func setupViews() {
+        view.addSubview(movieImageView)
+        view.addSubview(movieName)
+        view.addSubview(movieDEscription)
+        
+        NSLayoutConstraint.activate([
+            movieImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            movieImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            movieImageView.heightAnchor.constraint(equalToConstant: 200),
+            movieImageView.widthAnchor.constraint(equalToConstant: 300),
+            
+            movieName.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            movieName.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            movieName.topAnchor.constraint(equalTo: movieImageView.bottomAnchor, constant: 20),
+            
+            movieDEscription.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            movieDEscription.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            movieDEscription.topAnchor.constraint(equalTo: movieName.bottomAnchor, constant: 20),
+        ])
+    }
+}
+
+extension DetailView: DetailPresenterUI {
+    func updateUI(viewModel: DetailMovieViewModel) {
+        movieImageView.kf.setImage(with: viewModel.backdropPath)
+        movieName.text = viewModel.title
+        movieDEscription.text = viewModel.overview
+    }
 }
