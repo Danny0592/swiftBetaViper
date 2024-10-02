55be028b9fc876dffbdcfcb94dae78bf8fc63f29
Danny0592 <Dortiz.millan@gmail.com>
2024-10-02 01:44:42 -0600

    ListOfMoviesRouter

--- swiftBetaViper/ListOfViper/ListOfMoviesRouter.swift
+++ swiftBetaViper/ListOfViper/ListOfMoviesRouter.swift
@@ -7,20 +7,44 @@
 import Foundation
 import UIKit
 
-class ListOfMoviesRouter {
+protocol ListOfMoviesRouting: AnyObject{
+    var detailRouter: DetailRouting? { get }
+    var listOfMoviesView: ListOfMoviesView? { get }
+    
+    func showListOfMovies(window: UIWindow?)
+    func showDetailMovie(withMovieId movieId: String)
+}
+
+class ListOfMoviesRouter: ListOfMoviesRouting {
+    var detailRouter: DetailRouting?
+    var listOfMoviesView: ListOfMoviesView?
+    
+    
     func showListOfMovies(window: UIWindow?) {
         let interactor = ListOfMoviewInteractor()
-//        let interactor = ListOfMoviesInteractorMock()
+        //        let interactor = ListOfMoviesInteractorMock()
         let presenter = ListOfMoviesPresenter(listOfMoviesInteractor: interactor)
-      
-        let view = ListOfMoviesView(presenter: presenter)
-        presenter.ui = view
-//        view.presenter = presenter
         
-        window?.rootViewController = view
+        listOfMoviesView = ListOfMoviesView(presenter: presenter)
+        presenter.ui = listOfMoviesView
+        //        view.presenter = presenter
+        
+        window?.rootViewController = listOfMoviesView
         window?.makeKeyAndVisible()
     }
+    
+    func showDetailMovie(withMovieId movieId: String) {
+        guard let fromViewController = listOfMoviesView else {
+            return
+        }
+        
+        detailRouter?.showDetail(fromViewViewController: fromViewController,
+                                 withMovieId: movieId)
+    }
 }
+
+
+
 
 // CODIGO ORIGINAL
 // class ListOfMoviesRouter {
