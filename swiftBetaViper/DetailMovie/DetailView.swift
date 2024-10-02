fdeb544558ffda3b247fbc11401c4358767a9b4c
Danny0592 <Dortiz.millan@gmail.com>
2024-10-01 20:29:12 -0600

    detailView

--- swiftBetaViper/DetailMovie/DetailView.swift
+++ swiftBetaViper/DetailMovie/DetailView.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+class DetailView: UIViewController {
+    private let presenter: DetailPresentable
+    
+    
+    let movieImageView: UIImageView = {
+        let imageView =  UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints
+        return imageView
+    }()
+    
+    let movieName: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = .systemFont(ofSize: 32, weight: .bold, width: .condensed)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let movieDEscription: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 32, weight: .bold, width: .standard)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    
+    
+    init(presenter: DetailPresentable) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
